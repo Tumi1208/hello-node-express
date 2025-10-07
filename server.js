42fcbf430cdbb9bad387726941adc9ae43ebd995
Tumi <23521181@gm.uit.edu.vn>
2025-10-07 12:51:48 +0700

    Add HTML/CSS front-end for Express app

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello bel bel!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 app.get('/api/ping', (req, res) => {
   res.json({ pong: true, time: new Date().toISOString() });
